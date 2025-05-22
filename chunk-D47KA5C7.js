a45c7202aa7dff91b21b9f2cbdb4cbbf85a1157d
Skyler Alarcon <alarconkell@gmail.com>
2025-05-21 22:12:46 -0300

    hotfix

--- chunk-D47KA5C7.js
+++
@@ -1,1 +0,0 @@
-import{g as t,ha as e,ia as r}from"./chunk-LXDWCUS2.js";var v=(n,a)=>{let i=t(r),o=t(e);return i.isAdmin()?!0:(o.navigate.toLogin(),!1)};export{v as a};
---
+++ chunk-OZ6OQZYM.js
@@ -0,0 +1,1 @@
+import{g as t,ha as e,ia as r}from"./chunk-N2ZG4KML.js";var v=(n,a)=>{let i=t(r),o=t(e);return i.isAdmin()?!0:(o.navigate.toLogin(),!1)};export{v as a};
