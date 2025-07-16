19d3f5b787034680182b48a3e5a8d8a7b394ea13
Skyler Alarcon <alarconkell@gmail.com>
2025-07-15 22:38:58 -0300

    commit

--- chunk-2UUMW37B.js
+++
@@ -1,1 +0,0 @@
-import{g as t,ia as e,ja as r}from"./chunk-6JEIGHQT.js";var v=(n,a)=>{let i=t(r),o=t(e);return i.isAdmin()?!0:(o.navigate.toLogin(),!1)};export{v as a};
---
+++ chunk-OWGQGZCD.js
@@ -0,0 +1,1 @@
+import{g as t,la as e,ma as r}from"./chunk-KONNSNOE.js";var v=(n,a)=>{let i=t(r),o=t(e);return i.isAdmin()?!0:(o.navigate.toLogin(),!1)};export{v as a};
